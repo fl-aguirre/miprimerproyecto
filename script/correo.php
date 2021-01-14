a83288ba3d5ab507c71ff9ab3412ea17a74c7a8f
Franco Aguirre <frl.aguirre@gmail.com>
2021-01-14 20:44:23 -0300

    Nuevo link de redes y mejoras al PHP

--- script/correo.php
+++ script/correo.php
@@ -1,15 +1,17 @@
 <?php
-    if(isset($_POST['enviar'])){//si el botón con el name enviar es pulsado
-        if(!empty($_POST['nombre'])&& !empty($_POST['email'])&&!empty($_POST['msg'])){
-        //si no están vacios los inputs name, email y msg 
-        //guardo el contenido de cada campo en variables
+    if(isset($_POST['enviar'])){
+        if(!empty($_POST['nombre'])&& !empty($_POST['apellido'])&& !empty($_POST['email'])&&!empty($_POST['msg'])){
             $nombre=$_POST['nombre'];
             $apellido=$_POST['apellido'];
             $email=$_POST['email'];
             $red=$_POST['red'];
-            $news=$_POST['news'];
+            if($_POST['news']=='si'){
+                $news=$_POST['news'];
+            }else{
+                $news='no';
+            }
             $asunto=$_POST['asunto'];//puedo poner un input asunto o crearlo yo directamente  
-            $msg="Nombre: ".$nombre." Apellido: ".$apellido."\n"."Novevades: ".$news." Red: ".$red."\n".$_POST['msg'];
+            $msg="Nombre: ".$nombre."\n"."Apellido: ".$apellido."\n"."Novedades: ".$news."\n"."Red: ".$red."\n".$_POST['msg'];
             $header="From: ".$email."\r\n";//la persona que escribió me dejo su email, entonces el remitente es ese email
             $header.="Reply-To: noreply@example.com"."\r\n";//Le mando un no responder o noreply
             $header.="X-Mailer: PHP/".phpversion();
